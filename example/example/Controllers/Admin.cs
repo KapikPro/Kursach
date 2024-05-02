ac9a91b8a02345e34562607007161eb422972f37
Yaroslav Senchin <yrik200315@gmail.com>
2024-05-02 14:57:40 +0300

    Added roles(updated views and controllers)

---
+++ example/example/Controllers/Admin.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace example.Controllers
+{
+    public class Admin : Controller
+    {
+        [Authorize(Roles ="Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
